55465e66b243be0c19e20430f9842e6fe97940b4
tarou652 <rrrr20050307@gmail.com>
2024-06-26 10:29:06 +0900

    .envを作った

--- itivezi/src/firebase/firebase.ts
+++ itivezi/src/firebase/firebase.ts
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
 import { getDatabase } from 'firebase/database'
 const firebaseConfig = {
-  apiKey: 'AIzaSyARYmwkEzz0WY6LCzToU0t9Kb2uXirbfVI',
-  authDomain: 'blaze-lab-75c62.firebaseapp.com',
-  databaseURL: 'https://blaze-lab-75c62-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'blaze-lab-75c62',
-  storageBucket: 'blaze-lab-75c62.appspot.com',
-  messagingSenderId: '13282407977',
-  appId: '1:13282407977:web:1eafa1878605cebbab8e38',
-  measurementId: 'G-YF1LT22NK7'
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  databaseURL: import.meta.env.VITE_URL
 }
 console.log(firebaseConfig)
 // firebaseConfigの値を元にfirebaseの初期化
