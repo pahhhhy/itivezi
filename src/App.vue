747240f2039aaa54f8a2d71b2af7d116a85e5897
tarou652 <rrrr20050307@gmail.com>
2024-09-17 23:15:07 +0900

    AppHeaderのみデザインを手直しした。アニメーションにはGSAPを使うことにした

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <script setup lang="ts">
 import AppHeader from './components/AppHeader.vue'
+import { ref} from 'vue'
+import gsap from 'gsap';
+const isBurger=ref<boolean>()
+const background=ref(null)
+function OnBurger(bool:boolean){
+  if(!isBurger.value){
+    gsap.fromTo(background.value,0.5,{backgroundColor:"#f8f8f800"},{backgroundColor:"#00000020"})
+  }else{
+    gsap.fromTo(background.value,0.5,{backgroundColor:"#00000020"},{backgroundColor:"#f8f8f800"})
+  }
+  isBurger.value=bool;
+}
 </script>
 
 <template>
-  <AppHeader></AppHeader>
-
-  <RouterView />
+  <AppHeader v-on:-on-burger="OnBurger"></AppHeader>
+  <div v-bind:class="{black_back:isBurger}" class="background" ref="background">
+    <RouterView />
+  </div>
+  
 </template>
 
 <style scoped>
 * {
   width: 100%;
   height: 100%;
+}
+.background{
+  background-color:#f8f8f8 ;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+}
+.black_back{
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+  overflow:hidden;
 }
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,13 +10,27 @@ import {
 import { getDatabase, ref as fireRef, onValue } from 'firebase/database'
 import router from '@/router'
 import {useIconStore}from "../stores/icon"
+import gsap from 'gsap';
+interface Emits {
+  (event: 'OnBurger', Next: boolean): void
+}
+const emit = defineEmits<Emits>()
 const iconStore = (useIconStore())
 const iconURL=ref<string|null|undefined>(iconStore.iconURL)
 const myRole = ref<string>("")
 const isBurger = ref(false)
 const currentUser = ref<User | null>(null)
+const sidebar=ref(null)
 const onClickBurger = (): void => {
+  if(!isBurger.value){
+    gsap.to(sidebar.value,{x:205,duration:0.5})
+  }else{
+    gsap.to(sidebar.value,{x:-205,duration:0.5})
+  }
   isBurger.value = !isBurger.value
+  
+  
+  emit("OnBurger",isBurger.value)
 }
 const auth = getAuth()
 function logout() {
@@ -74,7 +88,10 @@ watch(
 
 <template>
   <header>
-    <img src="..\\assets\\itivezilogo.png" alt="" />
+    <div class="header-icon">
+      <img src="..\\assets\\itivezilogo.png" alt="" />
+    </div>
+    
     
     <nav>
       <div v-if="iconURL != null&&iconURL != '' "><img v-bind:src="iconURL" alt="" class="aicon-image"></div>
@@ -84,48 +101,48 @@ watch(
       <i v-if="isBurger" v-on:click="onClickBurger" class="bi bi-x-lg burger"></i>
     </nav>
   </header>
-  <aside v-if="isBurger">
+  <aside  ref="sidebar">
     <ul>
       <li>
         <button v-on:click="onClickBurger" class="sidebar_element">
-          <i class="bi bi-journals"></i>
-          <RouterLink v-bind:to="{ name: 'top' }" class="link">Top</RouterLink>
+          <i class="bi bi-house"></i>
+          <RouterLink v-bind:to="{ name: 'top' }" class="link">ホーム</RouterLink>
         </button>
       </li>
 
       <li v-if="currentUser != null">
         <button v-on:click="onClickBurger" class="sidebar_element">
-          <i class="bi bi-journals"></i>
+          <i class="bi bi-cart"></i>
           <RouterLink v-bind:to="{ name: 'order' }" class="link">注文</RouterLink>
         </button>
       </li>
       <li v-if="currentUser != null">
         <button v-on:click="onClickBurger" class="sidebar_element">
-          <i class="bi bi-journals"></i>
+          <i class="bi bi-pencil-square"></i>
           <RouterLink v-bind:to="{ name: 'registration' }" class="link">登録</RouterLink>
         </button>
       </li>
       <li v-if="currentUser != null">
         <button v-on:click="onClickBurger" class="sidebar_element">
-          <i class="bi bi-journals"></i>
+          <i class="bi bi-person"></i>
           <RouterLink v-bind:to="{ name: 'my-page' }" class="link">マイページ</RouterLink>
         </button>
       </li>
       <li v-if="currentUser == null">
         <button v-on:click="onClickBurger" class="sidebar_element">
-          <i class="bi bi-journals"></i>
+          <i class="bi bi-box-arrow-in-right"></i>
           <RouterLink v-bind:to="{ name: 'login' }" class="link">ログイン/新規登録</RouterLink>
         </button>
       </li>
       <li v-if="currentUser != null && myRole == '管理者'">
         <button v-on:click="onClickBurger" class="sidebar_element">
-          <i class="bi bi-journals"></i>
+          <i class="bi bi-columns-gap"></i>
           <RouterLink v-bind:to="{ name: 'Owner' }" class="link">管理者画面</RouterLink>
         </button>
       </li>
       <li v-if="currentUser != null">
         <button v-on:click="onClickBurger" class="sidebar_element" style="display: flex">
-          <i class="bi bi-journals"></i>
+          <i class="bi bi-box-arrow-in-left"></i>
           <p @click="logout" class="link">ログアウト</p>
         </button>
       </li>
@@ -134,8 +151,12 @@ watch(
 </template>
 
 <style scoped>
+
 body {
   position: relative;
+}
+ul{
+  padding: 0;
 }
 header {
   height: 80px;
@@ -144,48 +165,60 @@ header {
   justify-content: space-between;
   border-bottom: 1px solid rgb(223, 223, 223);
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.26);
+  padding: 0 3%;
+}
+.header-icon{
+  width: 50%;
 }
 nav {
+  width: 50%;
   display: flex;
   align-items: center;
-  justify-content: center;
-  padding: 10px;
+  justify-content: right;
+  padding: 5px;
 }
 .aicon-image{
   width: 50px;
   height: 50px;
   border-radius: 25px;
+  margin: 0 10px;
+}
+nav >p{
+  margin: 0 10px;
+  font-size: large;
+  font-weight:normal ;
+  color:var(--text-color);
 }
 .burger {
-  font-size: 40px;
-  color: greenyellow;
+  font-size: 50px;
+  color: var(--main-color);
 }
 aside {
-  height: 100%;
+  left: -205px;
+  height: calc(100% - 80px);
   width: 200px;
-  position: fixed;
-  border-right: 3px solid black;
+  position: absolute;
   background-color: white;
   z-index: 1;
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.25);
 }
 aside i {
-  font-size: 20px;
-  color: green;
+  font-size: 30px;
+  margin: 0 10px ;
+  color: var(--text-color);
 }
-aside ul {
-  font-size: 18px;
-  padding: 0;
-  color: green;
-}
+
 aside li {
   list-style: none;
   padding: 10px 5px 10px 10px;
-  border-top: 1px solid grey;
-  border-bottom: 1px solid grey;
+  border-top: 1px solid rgba(128, 128, 128, 0.24);
+  border-bottom: 1px solid rgba(128, 128, 128, 0.24);
 }
 .link {
   text-decoration: none;
-  color: black;
+  font-size: 24px;
+  padding: 0;
+  color: var(--other-color);
 }
 .sidebar_element {
   border: none;
