b82a6bc6c0e5ce19f2339514e07524fa76aab35c
tarou652 <rrrr20050307@gmail.com>
2025-01-14 21:24:09 +0900

    デプロイできるところまで

--- src/views/components/common/smallTextInputField.vue
+++ src/views/components/common/smallTextInputField.vue
@@ -1,118 +1,123 @@
 <script setup lang="ts">
-const model = defineModel<string>() // refではなくdefineModelにすることでこのコンポーネントのv-modelを定義できる
+import {ref} from "vue";
+import type HTMLInputEvent from "@/types/common/HTMLInputEvent";
 
-// props
+const message = defineModel<string>('message', {default: ""}); // refではなくdefineModelにすることでこのコンポーネントのv-modelを定義できる
+const isSending = defineModel<boolean>('isSending');
+const attachmentFiles = defineModel<File[]>('attachmentFiles', {default: undefined});
+
 interface Props {
-  onSendClicked: () => void
+  isTextMode?: boolean
+  placeholder: string
 }
 
-const {onSendClicked} = defineProps<Props>()
+const {isTextMode, placeholder} = defineProps<Props>()
 
-const setTextInputMatchHeight = () => {
-  // https://qiita.com/mtoutside/items/8dce5699a2d0283664a2
 
-  const textContents = document.getElementById('messageInput') as HTMLTextAreaElement;
-  if (!textContents) return;
-  // テキスト要素の高さを取得
-  const textHeight = textContents.clientHeight;
-  // テキスト要素のline-heightを取得
-  let lineHeight = getComputedStyle(textContents).getPropertyValue('line-height');
-  // [32.4px]のようなピクセル値が返ってくるので、数字だけにする
-  lineHeight = lineHeight.replace(/[^-\d.]/g, '');
-  // テキスト要素の行数を取得
-  const lines = textContents.value.split('\n').length;
-  // テキスト要素の高さを行数に応じて変更
-  // textContents.style.height = `${textHeight + (lines - 1) * lineHeight}px`;
-  // textareaのrows属性を行数に応じて変更
-  textContents.rows = lines;
+interface Emits {
+  (event: 'fileChange', e: HTMLInputEvent | DragEvent): void,
+
+  (event: 'send'): void
 }
 
+const emit = defineEmits<Emits>()
+
+const send = () => {
+  emit('send')
+  inputFile!.value = null
+  inputImg!.value = null
+}
+
+const inputImg = ref<null | HTMLInputElement>(null);
+const inputFile = ref<null | HTMLInputElement>(null);
 </script>
 <template>
-  <div class="messageBox">
-
-    <textarea rows="1" required placeholder="コメントを入力..." id="messageInput"
-              @focus="setTextInputMatchHeight()" @keyup="setTextInputMatchHeight()" @keydown="setTextInputMatchHeight()" @change="setTextInputMatchHeight"  v-model="model"/>
-    <button id="sendButton" @click="onSendClicked">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 664 663">
-        <path
-          fill="none"
-          d="M646.293 331.888L17.7538 17.6187L155.245 331.888M646.293 331.888L17.753 646.157L155.245 331.888M646.293 331.888L318.735 330.228L155.245 331.888"
-        ></path>
-        <path
-          stroke-linejoin="round"
-          stroke-linecap="round"
-          stroke-width="33.67"
-          stroke="#6c6c6c"
-          d="M646.293 331.888L17.7538 17.6187L155.245 331.888M646.293 331.888L17.753 646.157L155.245 331.888M646.293 331.888L318.735 330.228L155.245 331.888"
-        ></path>
-      </svg>
+  <div class="input-area">
+    <!-- 添付ファイル -->
+    <div v-if="!isTextMode">
+      <input
+          type="file"
+          multiple
+          @change="emit('fileChange', $event as HTMLInputEvent | DragEvent)"
+          id="file-input"
+          ref="inputFile"
+          style="display: none;"
+          accept="application/pdf, video/*, image/*, audio/*"
+      />
+      <button type="button" @click="inputFile!.click();">
+        <IconClip/>
+      </button>
+    </div>
+    <!-- 画像 -->
+    <div v-if="!isTextMode">
+      <input
+          type="file"
+          accept="image/*"
+          multiple
+          @change="emit('fileChange', $event as HTMLInputEvent | DragEvent)"
+          id="img-input"
+          ref="inputImg"
+          style="display: none;"
+      />
+      <button type="button" @click="inputImg!.click();">
+        <IconPhoto/>
+      </button>
+    </div>
+    <!-- テキスト -->
+    <textarea type="text" v-model="message" :placeholder="placeholder" :disabled="isSending"></textarea>
+    <!-- 送信ボタン -->
+    <button class="send-button" @click="send" type="button"
+            :disabled="isSending || !(message || (attachmentFiles && attachmentFiles.length))">
+      <IconSend :color="isSending || !(message || (attachmentFiles && attachmentFiles.length)) ? 'gray' : '#000'"/>
     </button>
   </div>
 
 </template>
 <style scoped>
-.messageBox {
-  width: 100%;
-  min-height: 40px;
-  height: fit-content;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #f8f8f8;
-  padding: 0 15px;
-  border-radius: 20px;
-  border: 1px solid rgb(110, 110, 110);
-}
 
-.messageBox:focus-within {
-  border: 1px solid rgb(63, 63, 63);
-}
-
-#messageInput {
-  padding: 10px;
-  width: 100%;
-  scrollbar-width: none;
-  background-color: transparent;
-  outline: none;
+button {
+  background: none;
   border: none;
-  color: black;
+}
+
+.input-area {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: max-content;
+  margin: 0 0 16px;
+  padding-top: 16px;
+}
+
+textarea {
+  /* textareaは自動拡張させる */
   resize: none;
-
-}
-
-#messageInput:focus ~ #sendButton svg path,
-#messageInput:valid ~ #sendButton svg path {
-  fill: #f2f2f2;
-  stroke: black;
-}
-
-
-#sendButton {
-  width: fit-content;
+  width: 100%;
   height: 100%;
-  background-color: transparent;
-  outline: none;
-  border: none;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  transition: all 0.3s;
+  min-height: 40px;
+  max-height: 160px;
+  field-sizing: content;
+  border-radius: 20px;
+  padding: .4em 1em;
+
+
+  & [disabled] {
+    background-color: #f0f0f0;
+  }
 }
 
-#sendButton svg {
-  height: 18px;
-  transition: all 0.3s;
-}
+.send-button {
+  position: relative;
+  z-index: 1;
 
-#sendButton svg path {
-  transition: all 0.3s;
-}
+  svg {
+    position: relative;
+    z-index: -1;
 
-#sendButton:hover svg path {
-  fill: #f2f2f2;
-  stroke: black;
+  }
+
 }
 
 </style>
