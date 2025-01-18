ebf7483aa8df0c387f155a9f20d60a921d9d664c
SHO800 <fg582323@docomo.ne.jp>
2025-01-18 16:24:29 +0900

    ユーザーアイコン取得ロジックのリファクタリング

--- src/utils/common/userData.ts
+++ src/utils/common/userData.ts
@@ -1,20 +1,19 @@
 import {getDownloadURL, getStorage, ref as storageRef} from "firebase/storage";
 import {readUserData} from "@/utils/database";
 
-export const getUserIconURL = (userId: string): Promise<string | null> => {
+export const getUserIconURL = async (userId: string): Promise<string | null> => {
     const storage = getStorage();
     const imageRef = storageRef(storage, `profile_pictures/${userId}`);
-    return getDownloadURL(imageRef).then((url) => {
-        return url;
-    }).catch((error) => {
+    let url = "";
+    try {
+        url = await getDownloadURL(imageRef);
+    } catch {
         const defaultImageRef = storageRef(storage, `profile_pictures/default.webp`);
-        return getDownloadURL(defaultImageRef).then((url) => {
-            return url;
-        }).catch((error) => {
-            console.error("Failed to get default image URL", error);
-            return null;
-        });
-    });
+        url = await getDownloadURL(defaultImageRef);
+
+    }
+
+    return url;
 }
 
 export const getUserName = (userId: string): Promise<string | null> => {
